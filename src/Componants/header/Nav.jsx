f4fe429223edfe422cd601da19f8d0fc5a2bfd50
SheikhSalman <assalmanmuhammad@gmail.com>
2025-05-20 17:32:38 +0600

    added tooltip on photoURL

--- src/Componants/header/Nav.jsx
+++ src/Componants/header/Nav.jsx
@@ -14,11 +14,11 @@ const Nav = () => {
 
     const handleLogout = () => {
         logOut()
-        .then(() => {
-           Swal.fire('user logged out successfully!')
-        }).catch((error) => {
-            toast.error(error.message)
-        });
+            .then(() => {
+                Swal.fire('user logged out successfully!')
+            }).catch((error) => {
+                toast.error(error.message)
+            });
     }
 
     return (
@@ -29,13 +29,16 @@ const Nav = () => {
                 </div>
 
                 <div className="navbar-end">
-                    <div className="hidden lg:flex">
+                    <div className="hidden lg:flex items-center">
                         <NavLink to='/'>Home</NavLink>
                         <NavLink to='/allgroup'>All Groups</NavLink>
                         <NavLink to='/create'>Create Group</NavLink>
                         <NavLink to='/mygroup'>My Groups</NavLink>
                         {
                             user ? <>
+                                <div className="tooltip" data-tip={user.displayName}>
+                                    <img className='w-14 h-14 rounded-full mr-1' src={user.photoURL} alt="" />
+                                </div>
                                 <button onClick={handleLogout} className='text-[12px] font-bold'>logout</button>
                             </>
                                 :
@@ -48,6 +51,9 @@ const Nav = () => {
                     </div>
                     <div className="dropdown lg:hidden">
                         <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                            {user && <div className="tooltip" data-tip={user.displayName}>
+                                <img className='w-10 h-10 rounded-full mr-1' src={user.photoURL} alt="" />
+                            </div>}
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
                         </div>
                         <ul
